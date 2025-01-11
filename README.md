4a2699c31a9ce075430095f72ca04388e4928d68
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-11 09:32:15 +0100

    Update to 5 rounds per game
    Replaces PlayRPS() with PlayRound();
    New PlayRPS() consists of for loop with 5 calls of
    PlayRound, keeping track of the score.
    Adjusted AskUser() to be less strict on input.

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ Version 1: Create a simple Javascript game within the console.
 ## How to play?
 
 Open console, type PlayRPS().
-A prompt window will pop up.
-You have 3 valid inputs: Rock/Paper/Scissors
-Case sensitive, no typo's.
-Good luck.
+You'll play 5 round:
+A prompt window will pop up for each round.
+
--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -4,36 +4,68 @@
 
 // Let the Machine decide a hand
 function RandomHand(){
-    let pcHand = Math.floor((Math.random()*3))
+    const pcHand = Math.floor((Math.random()*3))
     return pcHand
 }
 // Ask the User for their hand
 function AskUser(){
-    let userHand = prompt("Rock Paper Scissors?");
-    switch(userHand){
-        case "Rock": return 0;
-        case "Paper": return 1;
-        case "Scissors": return 2;
+    let userHand = prompt("Rock Paper Scissors?").toLowerCase();
+    switch(true){
+        case ((userHand=="rock") || (userHand=="r")): return 0;
+        case ((userHand=="paper") || (userHand=="p")): return 1;
+        case ((userHand=="scissors") || (userHand=="s")): return 2;
         default: console.log("Wrong input!"); AskUser();
     }
     return userHand;
 }
 // Check who won
 function CheckVictory(pcHand,userHand){
+
     if(pcHand==userHand){
-        console.log("Tie!")
+        console.log("Tie!");
+        return 0;
     }
     else if((pcHand==0 && userHand==1) || (pcHand==1 && userHand==2) || (pcHand==2 && userHand==0)){
-        console.log("You won!")
+        console.log("You won!");
+        return 1;
     }
     else{
-        console.log("You lost!")
+        console.log("You lost!");
+        return -1;
     }
 }
 
-// Initiate the game
+// Initiate the round
+function PlayRound(){
+    const pc = RandomHand();
+    const user = AskUser();
+    return userVictory = CheckVictory(pc,user);
+}
+
+//Checks Final winner
+function EvaluateWinner(userScore,pcScore){
+    if(userScore>pcScore){
+        console.log(`Congratulations! You won ${userScore} out of 5 games!`);
+    }
+    else if (userScore==pcScore){
+        console.log(`Ah a tie! You won ${userScore} games and lost ${pcScore}!`);
+    }
+    else{
+        console.log(`Sorry, you've lost ${pcScore} out of 5 games! Try again!`);
+    }
+}
+
 function PlayRPS(){
-    let pc = RandomHand();
-    let user = AskUser();
-    CheckVictory(pc,user);
+    let userScore =0;
+    let pcScore =0;
+    for(i=0;i<5;i++){
+        roundVal = PlayRound();
+        if(roundVal>0){
+            ++userScore;
+        }
+        else if(roundVal<0){
+            ++pcScore;
+        }
+    }
+    EvaluateWinner(userScore,pcScore);
 }
