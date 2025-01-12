27906212811b9b57aab9ea926a60b04a76465f0f
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-12 15:43:39 +0100

    Start version 2: rps-ui
    Update README.md to include current progress.
    Update index.html to include a div container.
    Update RockPaperScissor.js by:
        * Removing playRPS();
        * Changing function notation to camelCase;
        * Adding and calling domManipulation function.
            * Adds 3 buttons
            * Add Event with delegation.

--- README.md
+++ README.md
@@ -12,10 +12,16 @@ Except for the fact that I evaluate ints instead of "Rock","Paper" or "Scissor",
 ## Goal
 
 Version 1: Create a simple Javascript game within the console.
+Version 2: Create a UI for the game
 
 ## How to play?
 
-Open console, type PlayRPS().\
-You'll play 5 round:\
-A prompt window will pop up for each round.
+## To Do
+In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+
+    For now, remove the logic that plays exactly five rounds.
+    Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+    Add a div for displaying results and change all of your console.logs into DOM methods.
+    Display the running score, and announce a winner of the game once one player reaches 5 points.
+    You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
 
--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -3,23 +3,23 @@
 //------------------------------
 
 // Let the Machine decide a hand
-function RandomHand(){
+function randomHand(){
     const pcHand = Math.floor((Math.random()*3))
     return pcHand
 }
 // Ask the User for their hand
-function AskUser(){
+function askUser(){
     let userHand = prompt("Rock Paper Scissors?").toLowerCase();
     switch(true){
         case ((userHand=="rock") || (userHand=="r")): return 0;
         case ((userHand=="paper") || (userHand=="p")): return 1;
         case ((userHand=="scissors") || (userHand=="s")): return 2;
-        default: console.log("Wrong input!"); AskUser();
+        default: console.log("Wrong input!"); askUser();
     }
     return userHand;
 }
 // Check who won
-function CheckVictory(pcHand,userHand){
+function checkVictory(pcHand,userHand){
 
     if(pcHand==userHand){
         console.log("Tie!");
@@ -36,14 +36,14 @@ function CheckVictory(pcHand,userHand){
 }
 
 // Initiate the round
-function PlayRound(){
-    const pc = RandomHand();
-    const user = AskUser();
-    return userVictory = CheckVictory(pc,user);
+function playRound(){
+    const pc = randomHand();
+    const user = askUser();
+    return userVictory = checkVictory(pc,user);
 }
 
 //Checks Final winner
-function EvaluateWinner(userScore,pcScore){
+function evaluateWinner(userScore,pcScore){
     if(userScore>pcScore){
         console.log(`Congratulations! You won ${userScore} out of 5 games!`);
     }
@@ -55,17 +55,41 @@ function EvaluateWinner(userScore,pcScore){
     }
 }
 
-function PlayRPS(){
-    let userScore =0;
-    let pcScore =0;
-    for(i=0;i<5;i++){
-        roundVal = PlayRound();
-        if(roundVal>0){
-            ++userScore;
+function domManipulation(){
+    console.log("domManipulation Called")
+    // Select our container
+    const container = document.querySelector("div");
+
+    // Create 3 buttons
+    const btnRock = document.createElement("button");
+    btnRock.textContent = "Rock";
+    btnRock.setAttribute("id","r");
+    container.appendChild(btnRock);
+    const btnPaper = document.createElement("button");
+    btnPaper.textContent = "Paper";
+    btnPaper.setAttribute("id","p");
+    container.appendChild(btnPaper);
+    const btnScissors = document.createElement("button");
+    btnScissors.textContent = "Scissors";
+    btnScissors.setAttribute("id","s");
+    container.appendChild(btnScissors);
+
+    // Add EventHandling to container.
+    container.addEventListener('click', (event)=>{
+        let target=event.target;
+
+        switch(target.id){
+            case 'r':
+                console.log("rock");
+                break;
+            case 'p':
+                console.log("paper");
+                break;
+            case 's':
+                console.log("scissor");
+                break;
         }
-        else if(roundVal<0){
-            ++pcScore;
-        }
-    }
-    EvaluateWinner(userScore,pcScore);
+    })
 }
+
+domManipulation();
