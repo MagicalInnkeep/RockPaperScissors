cf8ac9ff94c609c8535ed80c7fa6474b21ff8e9b
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-12 20:08:47 +0100

    Update Version 2
    Expanded the javascript to display
        header with a roundCounter
        gameStatus which shows how you are fairing.
        Modified evaluateWinner and checkVictory to
            update an element.
        Modified getUserHand to validateUserInput.

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -7,52 +7,64 @@ function randomHand(){
     const pcHand = Math.floor((Math.random()*3))
     return pcHand
 }
-// Ask the User for their hand
-function askUser(){
-    let userHand = prompt("Rock Paper Scissors?").toLowerCase();
+// Validates User input.
+function validateUserInput(userHand){
     switch(true){
-        case ((userHand=="rock") || (userHand=="r")): return 0;
-        case ((userHand=="paper") || (userHand=="p")): return 1;
-        case ((userHand=="scissors") || (userHand=="s")): return 2;
-        default: console.log("Wrong input!"); askUser();
+        case ((userHand=="r")): return 0;
+        case ((userHand=="p")): return 1;
+        case ((userHand=="s")): return 2;
     }
     return userHand;
 }
 // Check who won
 function checkVictory(pcHand,userHand){
+    const pMessage = document.getElementById("pMessage");
 
     if(pcHand==userHand){
-        console.log("Tie!");
-        return 0;
+        pMessage.textContent="A tie!";
+        gamesTied = gamesTied+ 1;
     }
     else if((pcHand==0 && userHand==1) || (pcHand==1 && userHand==2) || (pcHand==2 && userHand==0)){
-        console.log("You won!");
-        return 1;
+        pMessage.textContent="You won!";
+        gamesWon = gamesWon+ 1;
     }
     else{
-        console.log("You lost!");
-        return -1;
+        pMessage.textContent="You lost!";
+        gamesLost = gamesLost+1;
     }
+
 }
 
 // Initiate the round
-function playRound(){
+function playRound(user){
     const pc = randomHand();
-    const user = askUser();
-    return userVictory = checkVictory(pc,user);
+    const userInput = validateUserInput(user);
+    checkVictory(pc,userInput);
 }
 
 //Checks Final winner
 function evaluateWinner(userScore,pcScore){
+    const pMessage = document.getElementById("pMessage");
     if(userScore>pcScore){
-        console.log(`Congratulations! You won ${userScore} out of 5 games!`);
+        pMessage.textContent=`Congratulations! You won ${userScore} out of 5 games!`;
     }
     else if (userScore==pcScore){
-        console.log(`Ah a tie! You won ${userScore} games and lost ${pcScore}!`);
+        pMessage.textContent=`Ah a tie! You won ${userScore} games and lost ${pcScore}!`;
     }
     else{
-        console.log(`Sorry, you've lost ${pcScore} out of 5 games! Try again!`);
+        pMessage.textContent=`Sorry, you've lost ${pcScore} out of 5 games! Try again!`;
     }
+}
+
+function updateHeader(){
+    const h1content = document.querySelector("h1");
+    h1content.textContent="Rock, Paper, Scissors! Round: "+roundCount;
+}
+
+function updateGameState(){
+    const gameStatus = document.getElementById("gameStatus");
+    gameStatus.textContent=`Games Won: ${gamesWon} \r\nGames Lost: ${gamesLost} \r\nGames Tied: ${gamesTied}`
+    
 }
 
 function domManipulation(){
@@ -60,6 +72,9 @@ function domManipulation(){
     // Select our container
     const container = document.querySelector("div");
 
+    const h1content = document.createElement("h1");
+    h1content.textContent="Rock, Paper, Scissors! Round: "+roundCount;
+    container.appendChild(h1content);
     // Create 3 buttons
     const btnRock = document.createElement("button");
     btnRock.textContent = "Rock";
@@ -74,22 +89,39 @@ function domManipulation(){
     btnScissors.setAttribute("id","s");
     container.appendChild(btnScissors);
 
+    const gameStatus = document.createElement("p");
+    gameStatus.style.whiteSpace="pre";
+    gameStatus.setAttribute("id","gameStatus");
+    gameStatus.textContent=`Games Won: ${gamesWon} \r\nGames Lost: ${gamesLost} \r\nGames Tied: ${gamesTied}`
+    container.appendChild(gameStatus);
+    const pMessage = document.createElement("p");
+    pMessage.setAttribute("id","pMessage");
+    container.appendChild(pMessage);
     // Add EventHandling to container.
     container.addEventListener('click', (event)=>{
         let target=event.target;
-
-        switch(target.id){
-            case 'r':
-                console.log("rock");
-                break;
-            case 'p':
-                console.log("paper");
-                break;
-            case 's':
-                console.log("scissor");
-                break;
+        roundCount=roundCount+1;
+        playRound(target.id);
+        updateGameState();
+        if (roundCount==6){
+            evaluateWinner(gamesWon,gamesLost);
+            container.removeChild(btnRock);
+            container.removeChild(btnPaper);
+            container.removeChild(btnScissors);
+        }
+        else{
+        updateHeader();
         }
     })
+
+
+
 }
 
+
+//Start the game.
+let roundCount =1;
+let gamesWon =0;
+let gamesLost =0;
+let gamesTied =0;
 domManipulation();
